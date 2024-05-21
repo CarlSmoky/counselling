b9c34526e446d5132bef4af3c4b75a397be18b38
tsumitakaoru <k.tsumita01@gmail.com>
2024-05-21 15:42:54 -0400

    Change style

--- app/_components/PageNotFound.tsx
+++ app/_components/PageNotFound.tsx
@@ -5,7 +5,7 @@ const PageNotFound:React.FC =()=> {
   return (
     <>
     <Navbar top={0}/>
-    <section className="margin-global mb-10 md:mb-16 lg:mb-20 xl:mb-24 min-h-[calc(100vh-475px)] relative">
+    <section className="margin-global mb-10 md:mb-16 lg:mb-20 xl:mb-24 min-h-[calc(100vh-380px)] lg:min-h-[calc(100vh-475px)] relative">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <p className="text-xl text-black-100/40">Page Not Found</p>
       </div>
