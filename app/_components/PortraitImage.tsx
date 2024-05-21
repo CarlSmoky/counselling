6204ad87384eeb1d1bd462574726512770257318
tsumitakaoru <k.tsumita01@gmail.com>
2024-05-21 16:49:07 -0400

    Style Portrait

--- app/_components/PortraitImage.tsx
+++ app/_components/PortraitImage.tsx
@@ -12,19 +12,20 @@ const PortraitImage: React.FC<Props> = async ({ src, alt }: Props) => {
   const buffer = await fs.readFile(`./public/${src}`);
   const { base64 } = await getPlaiceholder(buffer);
   return (
-    <div className="flex flex-col w-full sm:w-1/2 md:mt-0 md:w-1/3 m-auto ">
-      <div className="relative aspect-square w-full">
+    <div className="flex w-full sm:w-1/2 md:mt-0 md:w-1/3 m-auto">
+      <div className="flex flex-col bg-grey-300 mt-8 m-auto rounded-lg">
         <Image
-          className="object-contain mt-4 py-4"
+          className="object-contain m-auto p-4"
           src={src}
           alt={alt}
-          fill={true}
+          width={240}
+          height={300}
           sizes="(max-width: 480px) 66vw, (max-width: 768px) 50vw, 33vw"
           placeholder="blur"
           blurDataURL={base64}
         />
+        <p className="text-center text-sm text-black-100/60 p-3">Bo Yih Thom <br/>(Photo by Shandra Spears Bombay)</p>
       </div>
-      <p className="text-center text-sm text-black-100/60">Bo Yih Thom <br/>(Photo by Shandra Spears Bombay)</p>
     </div>
   );
 };
