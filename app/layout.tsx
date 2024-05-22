dcc1d83d39caeeaf8a905e4e8191e4ae21c98d86
tsumitakaoru <k.tsumita01@gmail.com>
2024-05-22 11:08:12 -0400

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,14 @@ import ContactInfoBar from "./_components/Header/ContactInfoBar"
 
 
 export const metadata: Metadata = {
-  title: "Integrative Counselling Resources",
-  description: "Registered Social Worker, Psychotherapist, and Clinical Supervisor that is based on racial liberation and anti-oppression principles in downtown Toronto",
+  title: {
+    default: "Integrative Counselling Resources",
+    template: "%s | Integrative Counselling Resources",
+  },
+  description: "Hi! I am Bo Yih Thom, Registered Social Worker, Psychotherapist, and Clinical Supervisor that is based on racial liberation and anti-oppression principles in Toronto",
 };
+
+
 
 interface childrenProps {
   children: React.ReactNode;
