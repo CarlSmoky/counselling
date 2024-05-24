919c20a67463a6236f2e843934d9e8e02ac7b8d7
tsumitakaoru <k.tsumita01@gmail.com>
2024-05-24 16:32:26 -0400

    Add X-Robots-Tag HTTP response header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Match all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow', 
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
