edeceabd7ee3f4191e528388f8856d30321c3ac3
tsumitakaoru <k.tsumita01@gmail.com>
2024-06-11 12:25:29 -0400

    Make sure the scrollToTop button is not focusable

--- app/_components/ScrollToTopButton.tsx
+++ app/_components/ScrollToTopButton.tsx
@@ -2,20 +2,27 @@
 import { IoIosArrowUp } from "react-icons/io";
 import { useScroll } from "../_hooks/useScroll";
 
-const ScrollToTopButton:React.FC = () => {
+const ScrollToTopButton: React.FC = () => {
   const isScrolled = useScroll();
 
   const handleScrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   return (
-    <div className={`${isScrolled ? 'opacity-100' : 'opacity-0'} fixed bottom-8 right-8 duration-300`}>
-      <button onClick={handleScrollToTop} aria-label="Scroll to top" className="flex w-14 h-14 bg-black-100/30 rounded-md">
-        <IoIosArrowUp className="text-white-100 m-auto text-2xl"/>
+    <div
+      className={`${isScrolled ? "opacity-100" : "opacity-0"} fixed bottom-8 right-8 duration-300`}
+    >
+      <button
+        onClick={handleScrollToTop}
+        aria-hidden="true"
+        tabIndex={-1} // Make sure the button is not focusable
+        className="flex h-14 w-14 rounded-md bg-black-100/30"
+      >
+        <IoIosArrowUp className="m-auto text-2xl text-white-100" />
         <span className="sr-only">Scroll to top</span>
       </button>
     </div>
